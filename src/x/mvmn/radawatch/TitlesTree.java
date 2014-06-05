95f89dfdb1656a1dacc8b1533b045c3d625c2870
mvmn <mvmn.inbox@gmail.com>
2014-06-05 18:33:17 +0300

    Draft version of titles analyzer - sort nodes by entry count

--- src/x/mvmn/radawatch/TitlesTree.java
+++ src/x/mvmn/radawatch/TitlesTree.java
@@ -1,7 +1,12 @@
 package x.mvmn.radawatch;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import javax.swing.JPanel;
 import javax.swing.JTree;
@@ -19,7 +24,24 @@ public class TitlesTree extends JPanel {
 
 	@SuppressWarnings("unchecked")
 	public void mapToNodes(DefaultMutableTreeNode parentNode, Map<String, Object> map) {
-		for (Map.Entry<String, Object> entry : map.entrySet()) {
+		List<Map.Entry<String, Object>> entries = new ArrayList<Map.Entry<String, Object>>(map.entrySet());
+		Collections.sort(entries, new Comparator<Map.Entry<String, Object>>() {
+
+			@Override
+			public int compare(Entry<String, Object> m1, Entry<String, Object> m2) {
+				int v1 = 0;
+				int v2 = 0;
+				if (m1.getValue() instanceof java.util.Map) {
+					v1 = (Integer) ((Map<String, Object>) m1.getValue()).get("__count");
+				}
+				if (m2.getValue() instanceof java.util.Map) {
+					v2 = (Integer) ((Map<String, Object>) m2.getValue()).get("__count");
+				}
+				return v2 - v1;
+			}
+
+		});
+		for (Map.Entry<String, Object> entry : entries) {
 			if (entry.getValue() instanceof java.util.Map) {
 				String nodeVal = String.format("%s [%s]", entry.getKey(), ((Map<String, Object>) entry.getValue()).get("__count"));
 				DefaultMutableTreeNode newNode = new javax.swing.tree.DefaultMutableTreeNode(nodeVal);
