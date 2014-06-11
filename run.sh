17b36b6d9ba6dd4030d41e6f2e0ae21e4623f37f
mvmn <mvmn.inbox@gmail.com>
2014-06-11 21:41:50 +0300

    Added -Xmx1024m to run.sh JVM options - more memory is needed to backup/restore larger DB

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 echo Note: You need Maven to run this
-mvn exec:java -Dexec.mainClass="x.mvmn.radawatch.RadaWatch"
+mvn exec:java -Dexec.mainClass="x.mvmn.radawatch.RadaWatch" -Dexec.args="-Xmx1024m"
+
