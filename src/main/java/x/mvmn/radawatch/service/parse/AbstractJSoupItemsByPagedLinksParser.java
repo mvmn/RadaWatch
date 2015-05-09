3828d0a91ad067e55cd819fbd8b67a01748e1cd3
mvmn <mvmn.inbox@gmail.com>
2015-05-10 00:42:10 +0300

    Use desktop browser useragent for HTTP requests.

--- src/main/java/x/mvmn/radawatch/service/parse/AbstractJSoupItemsByPagedLinksParser.java
+++ src/main/java/x/mvmn/radawatch/service/parse/AbstractJSoupItemsByPagedLinksParser.java
@@ -9,9 +9,11 @@ public abstract class AbstractJSoupItemsByPagedLinksParser<T extends Entity> imp
 
 	protected final int MAX_RETRIES = 3;
 	protected final int HTTP_TIMEOUT_MILLIS = 30000;
+	protected final int RETRY_DELAY = 5000;
+	protected final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/600.6.3 (KHTML, like Gecko) Version/8.0.6 Safari/600.6.3";
 
 	protected Document doGet(final String url) throws Exception {
-		return Jsoup.connect(url).timeout(HTTP_TIMEOUT_MILLIS).get();
+		return Jsoup.connect(url).userAgent(USER_AGENT).timeout(HTTP_TIMEOUT_MILLIS).get();
 	}
 
 	public Document get(final String url) throws Exception {
@@ -22,8 +24,9 @@ public abstract class AbstractJSoupItemsByPagedLinksParser<T extends Entity> imp
 				result = doGet(url);
 			} catch (final Exception e) {
 				if (tryNumber < MAX_RETRIES) {
-					System.err.println("Error fetching URL " + url + " (" + e.getClass().getName() + " " + e.getMessage() + ") - retrying (attempt #"
-							+ (tryNumber + 1) + ").");
+					System.err.println("Error fetching URL " + url + " (" + e.getClass().getName() + " " + e.getMessage() + ") - will retry after "
+							+ RETRY_DELAY / 1000 + " seconds (attempt #" + (tryNumber + 1) + ").");
+					Thread.sleep(RETRY_DELAY);
 				} else {
 					throw new Exception("Failed to get data with " + tryNumber + " retries from URL: " + url, e);
 				}
