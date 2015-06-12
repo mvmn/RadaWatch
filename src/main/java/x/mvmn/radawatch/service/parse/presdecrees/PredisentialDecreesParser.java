898fcc3d49a44a325894c29edbd26c4354ba30b1
mvmn <mvmn.inbox@gmail.com>
2015-06-12 03:03:13 +0300

    Updated presidential decrees parser according to recent changes to UA president website. Note: table presidentialdecree must be purged and all decrees re-fetched.

--- src/main/java/x/mvmn/radawatch/service/parse/presdecrees/PredisentialDecreesParser.java
+++ src/main/java/x/mvmn/radawatch/service/parse/presdecrees/PredisentialDecreesParser.java
@@ -10,8 +10,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -25,44 +23,58 @@ import x.mvmn.radawatch.service.parse.AbstractJSoupItemsByPagedLinksParser;
 
 public class PredisentialDecreesParser extends AbstractJSoupItemsByPagedLinksParser<PresidentialDecree> {
 
-	private final static String PAGE_URL_STR_PATTERN = "http://www.president.gov.ua/documents/index.php?start=%s&cat=-1&search_string=&logic=all&like=begin&number=&from_day=1&from_month=1&from_year=1994&till_day=31&till_month=12&till_year=2100&order=desc";
-	private final static String ITEM_URL_STR_PATTERN = "http://www.president.gov.ua/documents/%s.html?PrintVersion";
+	private final static String PAGES_LIST_URL = "http://www.president.gov.ua/documents/all?s-by=number&s-text=&contain-rule=starts&date-from=01-01-1990&date-to=01-01-2500";
+	private final static String PAGE_URL_STR_PATTERN = "http://www.president.gov.ua/documents/all?s-by=number&s-text=&contain-rule=starts&date-from=01-01-1990&date-to=01-01-2500&page=%s";
+	private final static String ITEM_URL_STR_PATTERN_RP = "http://www.president.gov.ua/documents/%s-rp-%s";
+	private final static String ITEM_URL_STR_PATTERN = "http://www.president.gov.ua/documents/%s-%s";
+	// private final static String PAGE_URL_STR_PATTERN =
+	// "http://www.president.gov.ua/documents/index.php?start=%s&cat=-1&search_string=&logic=all&like=begin&number=&from_day=1&from_month=1&from_year=1994&till_day=31&till_month=12&till_year=2100&order=desc";
+	// private final static String ITEM_URL_STR_PATTERN = "http://www.president.gov.ua/documents/%s.html?PrintVersion";
 	private int pageSize = 10;
-	private static final Pattern PAGE_ID_IN_URL_REGEX_PATTERN = Pattern.compile(".*/(\\d+)\\.html(?:\\?.*|$)");
+	// private static final Pattern PAGE_ID_IN_URL_REGEX_PATTERN = Pattern.compile(".*/(\\d+)\\.html(?:\\?.*|$)");
 
 	private final DateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy", new Locale("uk"));
 
 	@Override
 	public int parseOutTotalPagesCount() throws Exception {
-		Document firstPage = get(String.format(PAGE_URL_STR_PATTERN, 0));
-		pageSize = firstPage.select(".docs_list p:has(.head)").size();
-		return Integer.parseInt(firstPage.select(".page_split_bar a").last().text().trim());
+		Document firstPage = get(PAGES_LIST_URL);
+		// pageSize = firstPage.select(".docs_list p:has(.head)").size();
+		pageSize = firstPage.select("#res .doc_item").size();
+		// return Integer.parseInt(firstPage.select(".page_split_bar a").last().text().trim());
+		return Integer.parseInt(firstPage.select(".search_result_body .pagination li:nth-last-child(2) a").first().text().trim());
 	}
 
 	@Override
 	public List<ItemLinkData<PresidentialDecree>> parseOutItemsLinksData(int pageNumber) throws Exception {
-		final Document page = get(String.format(PAGE_URL_STR_PATTERN, (pageNumber - 1) * pageSize));
-		final Elements itemsHeadlines = page.select(".docs_list p:has(.head)");
+		final Document page = get(String.format(PAGE_URL_STR_PATTERN, pageNumber));
+		final Elements itemsHeadlines = page.select("#res .doc_item"); // page.select(".docs_list p:has(.head)");
 		final List<ItemLinkData<PresidentialDecree>> result = new ArrayList<ItemLinkData<PresidentialDecree>>(itemsHeadlines.size());
 		for (Element itemHeadlineElem : itemsHeadlines) {
-			final String type = cleanText(itemHeadlineElem.select(".head").first().ownText()).replaceAll("[ ]*№", "").trim();
-			String numberCode = "";
-			final Element numberCodeElem = itemHeadlineElem.select(".head b").first();
-			if (numberCodeElem != null) {
-				numberCode = cleanText(numberCodeElem.text()).trim();
-			}
-			final String dateStr = cleanText(itemHeadlineElem.select(".date").first().text()).trim();
-			final String title = cleanText(itemHeadlineElem.select("a").first().text()).trim();
-			final String href = cleanText(itemHeadlineElem.select("a").first().attr("href")).trim();
+			/*
+			 * final String type = cleanText(itemHeadlineElem.select(".head").first().ownText()).replaceAll("[ ]*№", "").trim(); String numberCode = ""; final
+			 * Element numberCodeElem = itemHeadlineElem.select(".head b").first(); if (numberCodeElem != null) { numberCode =
+			 * cleanText(numberCodeElem.text()).trim(); } final String dateStr = cleanText(itemHeadlineElem.select(".date").first().text()).trim(); final String
+			 * title = cleanText(itemHeadlineElem.select("a").first().text()).trim(); final String href =
+			 * cleanText(itemHeadlineElem.select("a").first().attr("href")).trim();
+			 * 
+			 * final Matcher idMatcher = PAGE_ID_IN_URL_REGEX_PATTERN.matcher(href); idMatcher.find(); final int id = Integer.parseInt(idMatcher.group(1));
+			 */
+			final String heading = cleanText(itemHeadlineElem.select("h3 a").first().ownText()).trim();
+			final String headingSplits[] = cleanText(heading).split("№");
+			final String type = headingSplits[0].trim();
+			final String numberCode = headingSplits[1].trim();
+			final String href = cleanText(itemHeadlineElem.select("h3 a").first().attr("href"));
+			final int hrefLastIndexOfDash = href.lastIndexOf("-");
+			final int id = Integer.parseInt(href.substring(hrefLastIndexOfDash + 1).trim());
+			final String title = cleanText(itemHeadlineElem.select(".doc_text").first().text()).trim();
+			final String dateStr = cleanText(itemHeadlineElem.select(".doc_date").first().text()).trim();
 
-			final Matcher idMatcher = PAGE_ID_IN_URL_REGEX_PATTERN.matcher(href);
-			idMatcher.find();
-			final int id = Integer.parseInt(idMatcher.group(1));
 			final ItemLinkData<PresidentialDecree> itemLinkData = new ItemLinkData<PresidentialDecree>(href, id);
 			itemLinkData.getAdditionalData().put("decreeType", type.toLowerCase());
 			itemLinkData.getAdditionalData().put("numberCode", numberCode);
 			itemLinkData.getAdditionalData().put("dateStr", dateStr);
 			itemLinkData.getAdditionalData().put("title", title);
+
 			result.add(itemLinkData);
 		}
 		return result;
@@ -88,10 +100,11 @@ public class PredisentialDecreesParser extends AbstractJSoupItemsByPagedLinksPar
 
 	@Override
 	public PresidentialDecree parseOutItem(ItemLinkData<PresidentialDecree> itemLinkData) throws Exception {
-		final Document page = get(String.format(ITEM_URL_STR_PATTERN, itemLinkData.getItemSiteId()));
+		final Document page = get(String.format(itemLinkData.getAdditionalData().get("decreeType").startsWith("розпорядження") ? ITEM_URL_STR_PATTERN_RP
+				: ITEM_URL_STR_PATTERN, itemLinkData.getAdditionalData().get("numberCode").replaceAll("[^0-9]+", ""), itemLinkData.getItemSiteId()));
 
 		final StringBuilder itemFullText = new StringBuilder();
-		page.select("#content").traverse(new NodeVisitor() {
+		page.select(".article_content .document_full").traverse(new NodeVisitor() {
 			@Override
 			public void tail(Node node, int depth) {
 				if (node instanceof TextNode) {
@@ -117,11 +130,10 @@ public class PredisentialDecreesParser extends AbstractJSoupItemsByPagedLinksPar
 	}
 
 	public static void main(String args[]) throws Exception {
-		System.out.println((int) ' ');
-		PredisentialDecreesParser testee = new PredisentialDecreesParser();
+		final PredisentialDecreesParser testee = new PredisentialDecreesParser();
 		System.out.println(testee.parseOutTotalPagesCount());
 		System.out.println(testee.pageSize);
-		for (ItemLinkData<PresidentialDecree> itemLinkData : testee.parseOutItemsLinksData(1)) {
+		for (final ItemLinkData<PresidentialDecree> itemLinkData : testee.parseOutItemsLinksData(1)) {
 			System.out.println(itemLinkData.toString());
 			System.out.println(testee.parseOutItem(itemLinkData));
 		}
